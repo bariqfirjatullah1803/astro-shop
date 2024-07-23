b60d601f30e380367adc2de13f44398d5227a500
Bariq Firjatullah <bariqfirjatullah1803@gmail.com>
2024-07-23 11:48:17 +0700

    Product showcase init

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import vercelServerless from '@astrojs/vercel/serverless';
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercelServerless({
-    imageService: true,
+    imageService: true
   }),
+  integrations: [tailwind()]
 });
